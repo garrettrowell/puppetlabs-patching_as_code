9e7a4048cdff4d235ba12ebd279124d48e91760f
Garrett Rowell <garrett@puppet.com>
2024-11-06 09:29:28 -0700

    more debug

--- manifests/windows/patchday.pp
+++ manifests/windows/patchday.pp
@@ -19,6 +19,7 @@ class patching_as_code::windows::patchday (
 ) {
   if $updates.count > 0 {
     $updates.each | $kb | {
+      notify { "should be installing: ${kb}": }
       patching_as_code::kb { $kb:
         ensure      => 'present',
         maintwindow => 'Patching as Code - Patch Window',
